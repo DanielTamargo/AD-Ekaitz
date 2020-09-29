d63dd680b4d217c060387d0148d2d56d1e7f297d
Daniel Tamargo (Clase) <danieltamargosaiz@gmail.com>
2020-09-29 20:29:23 +0200

    Next

---
+++ Ejercicios/Actividades Clase 4 - XML-20200929/ListaPersonas.java
@@ -0,0 +1,19 @@
+package accesodatos;
+
+import java.util.ArrayList;
+import java.util.List;
+public class ListaPersonas {
+
+    private List<Persona> lista = new ArrayList<Persona>();
+
+    public ListaPersonas(){
+    }
+
+    public void add(Persona per) {
+        lista.add(per);
+    }
+
+    public List<Persona> getListaPersonas() {
+        return lista;
+    }
+}
