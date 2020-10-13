e0269a5c8475877410065159ae5c6fd3e3a21644
Daniel Tamargo <danieltamargosaiz@gmail.com>
2020-10-14 01:49:36 +0200

    Practica1

--- Practicas/1-FinalFnatasy/src/com/tamargo/ventanas/NuevaPartida.java
+++ Practicas/1-FinalFnatasy/src/com/tamargo/ventanas/NuevaPartida.java
@@ -1,6 +1,8 @@
 package com.tamargo.ventanas;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class NuevaPartida {
     private JFrame ventanaNuevaPartida;
@@ -28,7 +30,54 @@ public class NuevaPartida {
         b_pj5.setIcon(new ImageIcon("assets/icono-5-asesino.png"));
 
         // Icono preview PJ
-        l_fotoPJ.setIcon(new ImageIcon("assets/pjs-0-ejemplo.png"));
+        l_fotoPJ.setIcon(new ImageIcon("assets/pjs-1-guerrero.png"));
+
+        b_pj1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cambiarDatos(1);
+            }
+        });
+        b_pj2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cambiarDatos(2);
+            }
+        });
+        b_pj3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cambiarDatos(3);
+            }
+        });
+        b_pj4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cambiarDatos(4);
+            }
+        });
+        b_pj5.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cambiarDatos(5);
+            }
+        });
+    }
+
+    public void cambiarDatos(int index) {
+        String foto = "assets/pjs-1-guerrero.png";
+        if (index == 1)
+            foto = "assets/pjs-1-guerrero.png";
+        else if (index == 2)
+            foto = "assets/pjs-2-mago.png";
+        else if (index == 3)
+            foto = "assets/pjs-3-arquero.png";
+        else if (index == 4)
+            foto = "assets/pjs-4-guardian.png";
+        else if (index == 5)
+            foto = "assets/pjs-5-asesino.png";
+
+        l_fotoPJ.setIcon(new ImageIcon(foto));
     }
 
     public void setVentanaNuevaPartida(JFrame ventanaNuevaPartida) {
